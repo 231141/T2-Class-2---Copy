e157b4884fac4dc07c44757d893bc30717f39795
231141 <231141@virtualwindow.co.za>
2024-04-30 12:58:39 +0200

    done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+console.log("I am " + 44 + " years old");
+console.log(87 + 32);
+
+let ticketNum = 3;
+const pi= 3.14;
+
+let firstName = "Clarissa" , surname = "Wang", faveNum= 44;
